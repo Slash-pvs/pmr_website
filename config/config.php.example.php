3f6145eb9505a145519022abea17f8f4424c9851
pierrevrgns_fixe <pierre.vrgns@gmail.com>
2025-06-24 14:25:16 +0200

    Retirer le dossier config du suivi Git et ignorer son contenu

--- config/config.php.example.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Exemple de configuration base de données
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'nom_de_ta_base');
-define('DB_USER', 'utilisateur');
-define('DB_PASS', 'mot_de_passe');
